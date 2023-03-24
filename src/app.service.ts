d625f4a7526b76a5a80657892ab1043a25dd40ee
Phạm Tấn Minh Tiến <phamtanminhtien@gmail.com>
2023-03-24 14:46:58 +0700

    modified

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World! ' + process.env.DB_NAME;
+  getHello() {
+    // return 'Hello World! ' + process.env.DB_NAME;
+    return {
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    };
   }
 }
